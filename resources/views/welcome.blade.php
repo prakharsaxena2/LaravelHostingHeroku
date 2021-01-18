d3871d739128e9d86086c582d6c79d04e00cfde4
Prakhar Saxena <prakhar.saxena@kelltontech.com>
2021-01-18 20:17:40 +0530

    auto deployment in heroku check

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Project Hosting on Heroku
                 </div>
 
                 <div class="links">
